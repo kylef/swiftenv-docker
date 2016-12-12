b61a6d36a95725ab1797d198b55aff260bea9461
Kyle Fuller <kyle@fuller.li>
2016-12-12 19:52:14 +0000

    fix(ci): Use long-form arguments to docker login

--- .travis-push.sh
+++ .travis-push.sh
@@ -3,6 +3,6 @@
 set -e
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then
-  docker login -u kylefci -p "$DOCKER_HUB_PASSWORD"
+  docker login --username kylefci --password "$DOCKER_HUB_PASSWORD"
   make push
 fi
